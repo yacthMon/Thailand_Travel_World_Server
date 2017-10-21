6eab872f52b4c3c7851e454933b4c9bb62259d3f
yacth_Mon <yacthmon@gmail.com>
2017-10-21 21:40:33 +0700

    Add monster controller

---
+++ Server/controllers/monsterController.js
@@ -0,0 +1,21 @@
+const Monster = require('./monster');
+
+const sampleSpawner = {
+    ID:0,
+    LocationSpanwer = {Start:{x:0,y:0} , End:{x:10,y:0}, Map:"Bangkok"},
+    MonsterID = 10000,
+    MaxAmount = 5
+}
+let monsterList = [];
+let spawnerList = [];
+
+spawnMonster = (spawner)=>{
+     //check if already full monster for this spawner yet ?
+     
+}
+
+let spawnInterval = setInterval(()=>{
+    spawnerList.forEach((spawner)=>{
+        spawnMonster(spawner);
+    })
+}, 60000); // Check and spawn new monster for every 1 minute
